d57d2bf9a95cbd292c819350e578744e9e9cb7b1
panos <panos.psimatikas@mystenlabs.com>
2025-12-10 16:17:29 -0300

    Delete sdk-easysui

--- sdk-easysui/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  extensionsToTreatAsEsm: ['.ts'],
-  testTimeout: 1000000,
-  transform: {
-    '^.+\\.ts$': ['ts-jest', { useESM: false }],
-  },
-};
