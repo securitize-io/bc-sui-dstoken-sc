e6b46ca13a86dc7d2da0e36ef06322d814efa630
panos <panos.psimatikas@mystenlabs.com>
2025-12-11 13:30:53 -0300

    Expose investors file as package

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,4 +1,5 @@
 export { create_ds_token } from './sdk/ds_token';
 export { Voloro } from './sdk/voloro';
 export * from './sdk/domains';
+export * from './sdk/investors';
 export * from './easysui';
