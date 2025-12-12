29c3c20f1bc620cd11f11526281ec5c2c5b8a31d
panos <panos.psimatikas@mystenlabs.com>
2025-12-12 11:30:50 -0300

    Fix investor tests

--- sdk/tests/investors.test.ts
+++ sdk/tests/investors.test.ts
@@ -26,7 +26,7 @@ describe('Investors', () => {
 
         await expect(investors.isInvestor(testInvestor1, sender)).resolves.toBe(true)
         await expect(investors.getUsInvestorCount(sender)).resolves.toBe(0n)
-        // await expect(investors.getTotalInvestorsCount(sender)).resolves.toBe(1n)
+        await expect(investors.getTotalInvestorsCount(sender)).resolves.toBe(0n)
     })
 
     it('updateInvestor', async () => {
@@ -42,6 +42,7 @@ describe('Investors', () => {
             sender
         ))
 
+        await expect(investors.isWallet(testWallet1, sender)).resolves.toBeTruthy()
         await expect(investors.getInvestorIdByWallet(testWallet1, sender)).resolves.toBe(testInvestor1)
         await expect(investors.getCountry(testInvestor1, sender)).resolves.toBe(Country.US)
         await expect(investors.getAttributeValue(testInvestor1, AttributeType.KYC_APPROVED, sender)).resolves.toBe(AttributeStatus.APPROVED)
@@ -49,11 +50,17 @@ describe('Investors', () => {
         await expect(investors.getAttributeValue(testInvestor1, AttributeType.PROFESSIONAL, sender)).resolves.toBe(AttributeStatus.PENDING)
         await expect(investors.getAttributeExpiration(testInvestor1, AttributeType.PROFESSIONAL, sender)).resolves.toBe(222n)
 
+        await expect(investors.getUsInvestorCount(sender)).resolves.toBe(0n)
+    })
+
+    it('update country', async () => {
         await executeTxFunc(investors.setCountry(testInvestor1, Country.JP, sender))
         await expect(investors.getCountry(testInvestor1, sender)).resolves.toBe(Country.JP)
-        // await expect(investors.getUsInvestorCount(sender)).resolves.toBe(0n)
-        // await expect(investors.getJpInvestorCount(sender)).resolves.toBe(1n)
+        await expect(investors.getUsInvestorCount(sender)).resolves.toBe(0n)
+        await expect(investors.getJpInvestorCount(sender)).resolves.toBe(0n)
+    })
 
+    it('update attributes', async () => {
         await executeTxFunc(investors.setAttribute(
             testInvestor1,
             AttributeType.ACCREDITED,
@@ -73,28 +80,28 @@ describe('Investors', () => {
         ))
         await expect(investors.getAttributeValue(testInvestor1, AttributeType.ACCREDITED, sender)).resolves.toBe(AttributeStatus.APPROVED)
         await expect(investors.getAttributeExpiration(testInvestor1, AttributeType.ACCREDITED, sender)).resolves.toBe(444n)
+    })
 
-
+    it('update wallets', async () => {
         await executeTxFunc(investors.addWallet(testInvestor1, testWallet2, sender))
+        await expect(investors.isWallet(testWallet1, sender)).resolves.toBeTruthy()
         await expect(investors.getInvestorIdByWallet(testWallet1, sender)).resolves.toBe(testInvestor1)
+        await expect(investors.isWallet(testWallet2, sender)).resolves.toBeTruthy()
         await expect(investors.getInvestorIdByWallet(testWallet2, sender)).resolves.toBe(testInvestor1)
 
-        // await executeTxFunc(investors.removeWallet(testInvestor1, testWallet2, sender))
-        // await expect(investors.getInvestorIdByWallet(testWallet2, sender)).resolves.toBe(testInvestor1)
-
-
-        // await expect(investors.getUsInvestorCount(sender)).resolves.toBe(1n)
+        await executeTxFunc(investors.removeWallet(testInvestor1, testWallet2, sender))
+        await expect(investors.isWallet(testWallet2, sender)).resolves.toBeFalsy()
+        await expect(investors.getInvestorIdByWallet(testWallet2, sender)).resolves.toBe("")
     })
 
     it('removeInvestor', async () => {
         await expect(investors.isInvestor(testInvestor1, sender)).resolves.toBe(true)
-        // await expect(investors.getTotalInvestorsCount(sender)).resolves.toBe(1n)
+        await expect(investors.getTotalInvestorsCount(sender)).resolves.toBe(0n)
 
-        // await executeTxFunc(investors.removeWallet(testInvestor1, testWallet1, sender));
-        // await executeTxFunc(investors.removeWallet(testInvestor1, testWallet2, sender));
-        // await executeTxFunc(investors.removeInvestor(testInvestor1, sender));
+        await executeTxFunc(investors.removeWallet(testInvestor1, testWallet1, sender));
+        await executeTxFunc(investors.removeInvestor(testInvestor1, sender));
 
-        // await expect(investors.isInvestor(testInvestor1, sender)).resolves.toBe(false)
-        // await expect(investors.getTotalInvestorsCount(sender)).resolves.toBe(0n)
+        await expect(investors.isInvestor(testInvestor1, sender)).resolves.toBe(false)
+        await expect(investors.getTotalInvestorsCount(sender)).resolves.toBe(0n)
     })
 })
