a2cf4513ae45a841c5ab546eb9b578766e18dfa6
panos <panos.psimatikas@mystenlabs.com>
2026-01-20 19:07:48 +0200

    Fix deploy outside of tests

--- sdk/tests/setup.ts
+++ sdk/tests/setup.ts
@@ -1,8 +1,6 @@
 import {PublishSingleton} from "../src";
-import {deployPas} from "../src/sdk/deploy_pas";
 
 
 beforeAll(async () => {
     PublishSingleton.cleanPubFile()
-    await deployPas() // TODO: Delete when pas is deployed in MVR
 })
