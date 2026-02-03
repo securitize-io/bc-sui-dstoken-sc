cd4b01e55d6323dee268631d8b645f12227b7f92
Haris Katimertzis <haris.katimertzis@mystenlabs.com>
2026-02-03 16:40:15 +0200

    fix: deploy

--- sdk/src/sdk/deploy_pas.ts
+++ sdk/src/sdk/deploy_pas.ts
@@ -2,7 +2,7 @@ import {ADMIN_KEYPAIR, PublishSingleton} from "../easysui";
 import {Config} from "./utils/config";
 
 export async function deployPas() {
-    const resp = await PublishSingleton.publishPackage(ADMIN_KEYPAIR!, '../move/pas')
+    const resp = await PublishSingleton.publishPackage(ADMIN_KEYPAIR!, '../move/pas/packages/pas')
     const packageId = PublishSingleton.findPublishedPackageId(resp)
     const upgradeCap = PublishSingleton.findUpgradeCapId(resp)
     const pasNamespace = PublishSingleton.findObjectIdByType(`${packageId}::namespace::Namespace`, true, resp)
