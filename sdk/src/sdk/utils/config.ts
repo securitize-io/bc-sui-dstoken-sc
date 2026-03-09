46b75cf4919f21e1832a38f8f8d8d184a428a999
Nikos Petridis <nick.petridis93@gmail.com>
2026-03-09 18:55:53 +0200

    feat: Resolve securitize and pas/ptb from git instead of local

--- sdk/src/sdk/utils/config.ts
+++ sdk/src/sdk/utils/config.ts
@@ -1,13 +1,13 @@
-import {Config as BaseConfig, BaseConfigVars, ExtraVarsMap} from '../../easysui'
+import { Config as BaseConfig, BaseConfigVars, ExtraVarsMap } from '../../easysui'
 
 interface ConfigVars extends BaseConfigVars {
-    SETUP_REGISTRY: string,
-    VERSION: string,
-    PAS_PACKAGE_ID: string,
-    PTB_PACKAGE_ID: string,
-    PAS_NAMESPACE: string,
-    TEMP_PATH: string,
-    SECURITIZE_TESTNET_ENV: string,
+    SETUP_REGISTRY: string
+    VERSION: string
+    PAS_PACKAGE_ID: string
+    PTB_PACKAGE_ID: string
+    PAS_NAMESPACE: string
+    TEMP_PATH: string
+    SECURITIZE_TESTNET_ENV: string
 }
 
 export class Config extends BaseConfig<ConfigVars> {
@@ -22,14 +22,14 @@ export class Config extends BaseConfig<ConfigVars> {
             PTB_PACKAGE_ID: process.env.PTB_PACKAGE_ID || '',
             PAS_NAMESPACE: process.env.PAS_NAMESPACE || '',
             TEMP_PATH: process.env.TEMP_PATH || './temp_tokens',
-            SECURITIZE_TESTNET_ENV: process.env.SECURITIZE_TESTNET_ENV || '',
+            SECURITIZE_TESTNET_ENV: process.env.SECURITIZE_TESTNET_ENV || 'testnet_alpha',
         }
     }
 
     static override get extraVars(): ExtraVarsMap {
         return {
-            SETUP_REGISTRY: "{packageId}::setup::SetupRegistry",
-            VERSION: "{packageId}::version::Version",
+            SETUP_REGISTRY: '{packageId}::setup::SetupRegistry',
+            VERSION: '{packageId}::version::Version',
         }
     }
 }
