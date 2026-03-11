1f5159f8a8938be2b6583f0cb59bf9755b8a4e19
Haris Katimertzis <haris.katimertzis@mystenlabs.com>
2026-03-11 20:06:04 +0200

    feat: change pas branch

--- sdk/src/sdk/templates/move_toml_template.ts
+++ sdk/src/sdk/templates/move_toml_template.ts
@@ -5,7 +5,7 @@ edition = "2024"
 
 [dependencies]
 securitize = { git = "git@github.com:securitize-io/bc-sui-dstoken-sc.git", subdir = "move/securitize", rev = "deployment-tests"  }
-pas = { git = "git@github.com:MystenLabs/pas.git", subdir = "packages/pas", rev = "ml/testnet-publish" }
+pas = { git = "git@github.com:MystenLabs/pas.git", subdir = "packages/pas", rev = "main" }
 
 [dep-replacements.testnet]
 securitize = { use-environment = "{TESTNET_ENV}" }
